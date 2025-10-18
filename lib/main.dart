16bafe64cbb367cbc9f1f26d27646ea24c05d373
Zahraa Sayed <zahraa@example.com>
2025-10-19 00:20:41 +0300

    Initial project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
